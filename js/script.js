1d892c2acdbc20d12fd11b9c64167798ee798299
esc <dennis@codinglab.com>
2023-10-19 03:01:13 +0200

    button speed

--- js/script.js
+++ js/script.js
@@ -76,7 +76,7 @@ document.addEventListener(
   "touchend",
   function (event) {
     const now = new Date().getTime();
-    if (now - lastTouchEnd <= 300) {
+    if (now - lastTouchEnd <= 100) {
       event.preventDefault();
     }
     lastTouchEnd = now;
